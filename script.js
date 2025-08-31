25353c43dd8bbb7d94fad82a730e2beae503928e
Gabriel Sebzi Davis <gabrieldavis1901@gmail.com>
2025-08-31 09:19:28 +0000

    deleting unessesary code

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 
-
-const hamburger = document.getElementById("hamburger")
-const menu = document.getElementById("menu")
-const navbar = document.querySelector(".navbar")
-
-hamburger.addEventListener("click", () => {
-    menu.style.display = (menu.style.display === "block") ? "none" : "block";
-});
-
-navbar.addEventListener("mouseenter", () => {
-    menu.style.display = "block";
-});
-
-navbar.addEventListener("mouseleave", () => {
-    menu.style.display = "block";
-});
-
